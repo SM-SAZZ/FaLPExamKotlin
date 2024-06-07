98e11a98435f82e5e8a6391e8ae8fa9310dba542
SAZZ <malakeev03@mail.ru>
2024-06-07 10:26:24 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.10"
+    application
+}
+
+group = "me.user"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("org.apache.poi:poi-ooxml:5.2.3")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "16"
+}
+
+application {
+    mainClass.set("MainKt")
+}
